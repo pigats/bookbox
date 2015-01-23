77cf950e902d09ed2ac1eef587af30fb48c8f155
pigats <andreapigato@gmail.com>
2015-01-23 23:11:56 +0000

    add dropbox auth flow (wip)

--- bookbox.rb
+++ bookbox.rb
@@ -1,18 +1,26 @@
 require 'sinatra'
 require 'sinatra/asset_pipeline'
+require 'dropbox_sdk'
 
 class BookBox < Sinatra::Base
   register Sinatra::AssetPipeline  
-
+  enable :session
+ 
   get '/' do 
-    @auth_url = 'https://www.dropbox.com/1/oauth2/authorize?client_id=0335l2g1v7ujyaa&response_type=code'
-
+    flow =  DropboxOAuth2Flow.new(ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET'], url('/auth/dropbox'), session, :csrf_token_session_key) 
+    session[:flow] = flow
+    @auth_url = flow.start 
     haml :index
   end
 
-  get 'auth/dropbox' do 
-    params.inspecs
+  get '/auth/dropbox' do 
+    session[:flow]
+    token = session[:flow].finish(params[:code])
+    #return token
   end
 
+  get '/env' do
+    ENV['DROPBOX_KEY']
+  end
 
 end
