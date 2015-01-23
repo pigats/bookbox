4579d99ff5dcdc635c3f23c484c2d247d4f06fcf
pigats <andreapigato@gmail.com>
2015-01-23 23:53:10 +0000

    complete dropbox auth flow

--- bookbox.rb
+++ bookbox.rb
@@ -1,26 +1,31 @@
 require 'sinatra'
 require 'sinatra/asset_pipeline'
 require 'dropbox_sdk'
+require 'pry'
 
 class BookBox < Sinatra::Base
   register Sinatra::AssetPipeline  
-  enable :session
- 
+  enable :sessions
+  
+  def dropbox_auth_flow
+    DropboxOAuth2Flow.new(ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET'], url('/signup/dropbox'), session, :dropbox_auth_csrf_token) 
+  end
+
   get '/' do 
-    flow =  DropboxOAuth2Flow.new(ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET'], url('/auth/dropbox'), session, :csrf_token_session_key) 
-    session[:flow] = flow
-    @auth_url = flow.start 
+    @auth_url = dropbox_auth_flow.start 
     haml :index
   end
 
-  get '/auth/dropbox' do 
-    session[:flow]
-    token = session[:flow].finish(params[:code])
-    #return token
+  get '/signup/dropbox' do 
+    access_token, user_id, url_state = dropbox_auth_flow.finish(params)
+    #save user
+    session[:dropbox_token] = access_token
+    redirect url('/signup/genre')
   end
 
-  get '/env' do
-    ENV['DROPBOX_KEY']
+  get '/signup/genre' do 
+    DropboxClient.new(session[:dropbox_token]).put_file('/test.jpg', open('./test.jpg'))  
+    'yay'
   end
 
 end
