2c9f5bb9bb89768b65ee1909ba33270e233b5f69
Alvise Susmel <alvise@mbp.local>
2015-01-24 01:32:46 +0000

    model fields

--- models/user.rb
+++ models/user.rb
@@ -2,9 +2,17 @@ class User
 	include Mongoid::Document
 	include Mongoid::Timestamps #created_at, updated_at
 
-	field :name, String
-	field :email, String
-	field :dropbox_id, String
-	field :dropbox_token, String
+	field :name, type: String
+	field :email, type: String
+	
+	field :dropbox_id, type: String
+	field :dropbox_token, type: String
+
+	field :read_books, type: Array
+	field :unliked_books, type: Array
+	
+	index({email: 1}, {unique: true})
+	index({dropbox_id: 1}, {unique: true})
+
 
 end
